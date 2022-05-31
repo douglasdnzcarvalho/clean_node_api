60c2bc19c8187dc4b9efb1b60e361100aaddbfcd
Douglas Diniz Carvalho <douglasdnzcarvalho@gmail.com>
2022-05-31 13:33:30 -0300

    feat: ensure returns content type as xml when forced

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -3,12 +3,23 @@ import app from '../config/app'
 
 describe('Content Type Middleware', () => {
   test('Should return default content type as json', async () => {
-    app.get('/test_content_type', (request, response) => {
+    app.get('/test_default_content_type', (request, response) => {
       response.send()
     })
 
     await request(app)
-      .get('/test_content_type')
+      .get('/test_default_content_type')
       .expect('content-type', /json/)
+  })
+
+  test('Should return xml content type when forced', async () => {
+    app.get('/test_content_type_xml', (request, response) => {
+      response.type('xml')
+      response.send()
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
   })
 })
