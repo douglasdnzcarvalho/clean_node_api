4beb4be1a9700377f9073e2ffa63ae7b59124399
Douglas Diniz Carvalho <douglasdnzcarvalho@gmail.com>
2022-06-08 17:57:09 -0300

    refactor: move signup factory files to a folder

---
+++ src/main/factories/signup/signup.ts
@@ -0,0 +1,18 @@
+import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
+import { BCryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
+import { SignUpController } from '../../../presentation/controllers/signup/signup'
+import { Controller } from '../../../presentation/protocols'
+import { LogControllerDecorator } from '../../decorators/log'
+import { makeSignUpValidation } from './signup-validation'
+
+export const makeSignUpController = (): Controller => {
+  const encrypter = new BCryptAdapter(12)
+  const accountRepository = new AccountMongoRepository()
+  const addAccount = new DbAddAccount(encrypter, accountRepository)
+  const validationComposite = makeSignUpValidation()
+  const controller = new SignUpController(addAccount, validationComposite)
+  const logErrorRepository = new LogMongoRepository()
+  return new LogControllerDecorator(controller, logErrorRepository)
+}
