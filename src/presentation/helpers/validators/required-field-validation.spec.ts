cf75083e66d824aaeeafdb4600c4d113680b4eb6
Douglas Diniz Carvalho <douglasdnzcarvalho@gmail.com>
2022-06-08 15:13:04 -0300

    test: ensure RequiredFieldValidation returns null if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -16,8 +16,13 @@ const makeSut = (): SutTypes => {
 describe('RequiredField Validation', () => {
   test('Should return a MissingParamError if validation fails', () => {
     const { sut } = makeSut()
-
     const error = sut.validate({})
     expect(error).toEqual(new MissingParamError('required_field'))
+  })
+
+  test('Should return null if validation succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ required_field: 'any_value' })
+    expect(error).toEqual(null)
   })
 })
