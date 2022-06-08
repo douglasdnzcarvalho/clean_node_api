8e06f5f75980e0bc5a7ab873427be2de35835eb9
Douglas Diniz Carvalho <douglasdnzcarvalho@gmail.com>
2022-06-08 15:15:13 -0300

    refactor: remove unused code

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,27 +1,19 @@
 import { MissingParamError } from '../../errors'
 import { RequiredFieldValidation } from './required-field-validation'
 
-interface SutTypes {
-  sut: RequiredFieldValidation
-}
-
-const makeSut = (): SutTypes => {
-  const sut = new RequiredFieldValidation('required_field')
-
-  return {
-    sut
-  }
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('required_field')
 }
 
 describe('RequiredField Validation', () => {
   test('Should return a MissingParamError if validation fails', () => {
-    const { sut } = makeSut()
+    const sut = makeSut()
     const error = sut.validate({})
     expect(error).toEqual(new MissingParamError('required_field'))
   })
 
   test('Should return null if validation succeeds', () => {
-    const { sut } = makeSut()
+    const sut = makeSut()
     const error = sut.validate({ required_field: 'any_value' })
     expect(error).toEqual(null)
   })
