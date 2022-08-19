7be3f871846a947acd010d7cdbd45e8c5f520891
Douglas Diniz Carvalho <douglasdnzcarvalho@gmail.com>
2022-08-19 18:51:45 -0300

    feat: implementation of DbAddSurvey

---
+++ tests/data/mocks/mock-db-survey.ts
@@ -0,0 +1,9 @@
+import { AddSurveyRepository } from '@/data/protocols/db/survey'
+
+export class AddSurveyRepositorySpy implements AddSurveyRepository {
+  params: AddSurveyRepository.Params
+
+  async add (params: AddSurveyRepository.Params): Promise<void> {
+    this.params = params
+  }
+}
