4a223b390ee177143b0a79fdf253328f970f50d4
Douglas Diniz Carvalho <douglasdnzcarvalho@gmail.com>
2022-08-19 16:34:54 -0300

    refactor: move files

--- src/main/factories/usecases/db-authentication-factory.ts
+++ src/main/factories/usecases/db-authentication-factory.ts
@@ -1,9 +1,9 @@
-import { DbAuthentication } from '@/data/usecases/authentication/db-authentication'
+import { DbAuthentication } from '@/data/usecases/db-authentication'
 import { Authentication } from '@/domain/usecases/authentication'
 import { BCryptAdapter } from '@/infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { JwtAdapter } from '@/infra/criptography/jwt-adapter/jwt-adapter'
 import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
-import env from '../../../config/env'
+import env from '../../config/env'
 
 export const makeDbAuthentication = (): Authentication => {
   const accountMongoRepository = new AccountMongoRepository()
