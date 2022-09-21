4f244bf3ae2fd625f7ebeacf7eec20ff273e6a21
Douglas Diniz Carvalho <douglasdnzcarvalho@gmail.com>
2022-09-21 19:01:25 -0300

    feat: implementation of DbLoadSurveys

--- tests/data/mocks/mock-db-survey.ts
+++ tests/data/mocks/mock-db-survey.ts
@@ -1,9 +1,20 @@
-import { AddSurveyRepository } from '@/data/protocols/db/survey'
+import { AddSurveyRepository, LoadSurveysRepository } from '@/data/protocols/db/survey'
+import { mockSurveyModels } from '@/tests/domain/mocks'
 
 export class AddSurveyRepositorySpy implements AddSurveyRepository {
   params: AddSurveyRepository.Params
 
   async add (params: AddSurveyRepository.Params): Promise<void> {
     this.params = params
+  }
+}
+
+export class LoadSurveysRepositorySpy implements LoadSurveysRepository {
+  accountId: string
+  result = mockSurveyModels()
+
+  async loadAll (accountId: string): Promise<LoadSurveysRepository.Result> {
+    this.accountId = accountId
+    return this.result
   }
 }
