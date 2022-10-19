5c2629a9ae92d771df3c03ddb4a7485e63e09d9b
Douglas Diniz Carvalho <douglasdnzcarvalho@gmail.com>
2022-10-19 12:54:55 -0300

    feat: serving static files

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import express, { Express } from 'express'
 import setupSwagger from '@/main/config/swagger'
+import setupStaticFiles from '@/main/config/static-files'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
 
 export const setupApp = async (): Promise<Express> => {
   const app = express()
+  setupStaticFiles(app)
   setupSwagger(app)
   setupMiddlewares(app)
   setupRoutes(app)
