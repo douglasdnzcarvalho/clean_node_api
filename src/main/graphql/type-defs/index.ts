67ae03111666ea75d08eb34323fca21e2566ff0a
Douglas Diniz Carvalho <douglasdnzcarvalho@gmail.com>
2022-12-03 13:35:39 -0300

    refactor: rename 'login' file to 'auth'

--- src/main/graphql/type-defs/index.ts
+++ src/main/graphql/type-defs/index.ts
@@ -1,6 +1,6 @@
 import base from './base'
-import login from './login'
+import auth from './auth'
 import survey from './survey'
 import surveyResult from './survey-result'
 
-export default [base, login, survey, surveyResult]
+export default [base, auth, survey, surveyResult]
